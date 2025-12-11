a6de5e789f6605b8c05efb813fb34793b547ab67
Hendry Tanaka <hendrytanaka10@icloud.com>
2025-12-11 12:10:35 +0700

    chore: add comparation to example

--- apps/schemas/role.ts
+++ apps/schemas/role.ts
@@ -27,3 +27,27 @@ export class Role {
   @Prop()
   status: string;
 }
+
+@Schema({
+  collection: "role_2",
+  timestamps: true,
+})
+export class RoleSecond {
+  /** Core data */
+  @Prop()
+  role_id: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: false })
+  description: string;
+
+  /** Core data */
+  @Prop()
+  authorizes: any[];
+
+  /** Core data */
+  @Prop()
+  status: string;
+}
